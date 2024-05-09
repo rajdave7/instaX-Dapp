c9a94ccb054b107b4353a93329ac7a7080d33b2b
CharmiDesai1 <charmi.d@ahduni.edu.in>
2024-05-09 18:34:12 +0530

    Added image folder

--- src/pages/MyPosts/MyPosts.jsx
+++ src/pages/MyPosts/MyPosts.jsx
@@ -73,26 +73,25 @@ const MyPosts = () => {
     <div className="MyPosts">
       <div className="container mt-4">
         <div className="row">
-          <div className="col-md-4">
-            <CreatePost />
+            <div className="col-md-7" >
+            <div style={{ paddingRight: '20px' }}>
+                <div className="form-group">
+                  <label htmlFor="sort" className="text-light card-dark" style={{ padding: '5px 10px', borderRadius: '5px' }}>Sort by: </label>
+                    <select
+                      id="sort"
+                      value={sortBy}
+                      onChange={handleSortChange}
+                      className="custom-select card-dark" style={{ marginLeft: '10px' , padding: '5px 10px', borderRadius: '5px' }}
+                    >
+                      <option value="desc">Newest First</option>
+                      <option value="asc">Oldest First</option>
+                    </select>
+                </div>
+              {posts.map((post, index) => (
+                <Post key={index} post={post} user={user} />
+              ))}
             </div>
-            <div className="col-md-5">
-            <div className="form-group">
-            <label htmlFor="sort" className="text-light card-dark" style={{ padding: '5px 10px', borderRadius: '5px' }}>Sort by: </label>
-              <select
-                id="sort"
-                value={sortBy}
-                onChange={handleSortChange}
-                className="custom-select card-dark" style={{ marginLeft: '10px' , padding: '5px 10px', borderRadius: '5px' }}
-              >
-                <option value="desc">Newest First</option>
-                <option value="asc">Oldest First</option>
-              </select>
             </div>
-            {posts.map((post, index) => (
-              <Post key={index} post={post} user={user} />
-            ))}
-          </div>
           <div className="col-md-3">
             <ConnectButton />
             <Navbar />
